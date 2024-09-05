d99be9ada02ce47abb125b6c2aa14d9be10ea59e
www222fff <feng73440@gmail.com>
2024-09-05 11:26:16 +0800

    update address

--- chrome_extension/address.js
+++ chrome_extension/address.js
@@ -1,1 +1,11 @@
-history.replaceState(null, '', '8443/szzh/zhcx/sbxx/sbxxcx?ruuid=1725491537839');
+const generateRandomNumber = (length) => {
+  const min = Math.pow(10, length - 1);
+  const max = Math.pow(10, length) - 1;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};  
+
+const randomNumber = generateRandomNumber(13);
+
+const newUrl = `:8443/szzh/zhcx/sbxx/sbxxcx?ruuid=${randomNumber}`;
+  
+history.replaceState(null, '', newUrl);
